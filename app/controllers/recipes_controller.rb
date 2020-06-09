d663920007529a56948cb0f3a6ea78daf5bdb52a
danielsun <danielsun12345@gmail.com>
2020-06-08 23:27:31 -0700

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,8 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+    @recipe.ingredients.build(ingredient_type: 'first')
+    @recipe.ingredients.build(ingredient_type: 'second')
   end
 
   def index
@@ -12,5 +14,13 @@ class RecipesController < ApplicationController
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
   end
+
+  private
+
+  def recipe_params
+      params.require(:recipe).permit(:title)
+  end 
+  
 end
