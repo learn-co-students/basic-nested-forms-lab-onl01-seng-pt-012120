0199c881837e4ad58ea1458a989aa0add4389582
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-06-12 20:14:13 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,26 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'ingredients_name')
+    @recipe.ingredients.build(quantity: 'ingredient_quantity')
   end
 
   def create
+    @recipe = Recipe.find_by(params[:id])
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attribute: [
+        :name,
+        :quantity,
+        :recipe_id
+      ]
+    )
   end
 end
