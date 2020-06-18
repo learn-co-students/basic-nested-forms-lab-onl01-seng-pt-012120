6265695ce79a0d567471a9fa074331aeca35ce84
github username <github email address>
2020-06-17 17:39:37 -0700

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  # def recipe_attributes=(ingredients)
+  #   self.ingredient = ingredient.find_or_create_by(name: ingredient[:name])
+  #   self.ingredient.update(ingredient)
+  # end
+ 
 end
